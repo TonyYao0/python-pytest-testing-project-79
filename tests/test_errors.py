d06a47b3c1471f0eb097ce0621ffe07d441fd961
TonyYao0 <tony_yao@mail.ru>
2026-02-02 19:29:50 +0800

    Исправления

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -15,18 +15,23 @@ def test_download_resource_404(requests_mock, tmp_path, caplog):
     )
     requests_mock.get(url, text=html_content)
     requests_mock.get(bad_img_url, status_code=404)
-    requests_mock.get(good_img_url, text=html_content)
-    
-    actual_path = Path(download(url, tmp_path))
+    requests_mock.get(good_img_url, text='image data')
+
+    with caplog.at_level("WARNING"):
+        actual_path = Path(download(url, tmp_path))
 
     assert "404" in caplog.text
+    assert "404.png" in caplog.text
+
     assert actual_path.name == "ru-hexlet-io-courses.html"
     content = actual_path.read_text()
     assert '/assets/404.png' in content
-    assert 'ru-hexlet-io-courses_files/ru-hexlet-io-assets-good.png' in content
-    assert 'ru-hexlet-io_files/ru-hexlet-io-assets-404.png' not in content
-    res_path = tmp_path / "ru-hexlet-io-courses_files" / "ru-hexlet-io-assets-404.png"
-    assert not res_path.exists()
+
+    res_dir = tmp_path / "ru-hexlet-io-courses_files"
+    assert not (res_dir / "ru-hexlet-io-assets-404.png").exists()
+    assert (res_dir / "ru-hexlet-io-assets-good.png").exists()
+    assert len(list(res_dir.iterdir())) == 1
+
 
 def test_download_network_error(requests_mock, tmp_path):
     url = "https://httpbin.org/status/404"
